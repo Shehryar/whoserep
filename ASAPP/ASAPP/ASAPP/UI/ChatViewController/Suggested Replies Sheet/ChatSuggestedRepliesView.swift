de2dac15a809719b0701e347a383cc7df95d1210
Mitchell Morgan <msmorgan90@gmail.com>
2016-08-14 22:19:27 -0400

    Adding sample data for srs integration

---
+++ ASAPP/ASAPP/ASAPP/UI/ChatViewController/Suggested Replies Sheet/ChatSuggestedRepliesView.swift
@@ -0,0 +1,16 @@
+//
+//  ChatSuggestedRepliesView.swift
+//  ASAPP
+//
+//  Created by Mitchell Morgan on 8/13/16.
+//  Copyright © 2016 asappinc. All rights reserved.
+//
+
+import UIKit
+
+class ChatSuggestedRepliesView: UIView {
+
+    var srsItem: [SRSItemListItem]?
+    
+
+}
