257429950703729173498e256b4e05cc304b6b80
Mitchell Morgan <mitch@asapp.com>
2017-05-04 17:48:50 -0400

    Updating file server

--- Components/component-server.js
+++ Components/component-server.js
@@ -1,90 +1,52 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-// you can pass the parameter in the command line. e.g. node static_server.js 3000
+const http = require("http");
+const url = require("url");
+const fileUtil = require("./file-util");
+
+// Can pass in the port number
 const port = process.argv[2] || 9000;
+const jsonDirectory = "./json/";
 
-const jsonDirectory = './json/';
 
-function getAvailableComponents(callback) {
-  fs.readdir(jsonDirectory, (err, files) => {
-    var components = []
-    files.forEach(file => {
-      if (~file.indexOf('.json')) {
-        components.push(file.split('.json')[0]);
-      }
-    });
-    callback(components);
-  })
+function getListOfJSONFiles(req, res) {
+  fileUtil.getFilesInDirectory(jsonDirectory, true, function(code, fileNames, err) {
+    if (code != 200) {
+      res.statusCode = code;
+      res.end(err);
+    } else {
+      res.setHeader("Content-type", "application/json");
+      res.end(JSON.stringify(fileNames));
+    }
+  });
 }
 
+function getFileContents(req, res) {
+  const pathname = jsonDirectory + url.parse(req.url).pathname;
+  fileUtil.getContentsOfFile(pathname, function(code, data, contentType, err) {
+    if (code != 200) {
+      res.statusCode = code;
+      res.end(err);
+    } else {
+      res.setHeader("Content-type", contentType);
+      res.end(data);
+    }
+  });
+}
 
-
-
+// HTTP Server
 http.createServer(function (req, res) {
 
-  console.log(`${req.method} ${req.url}`);
+  console.log(req.method + " " + req.url);
 
-  if (req.method == 'GET' && req.url == '/components') {
-    getAvailableComponents( function(components) {
-      res.setHeader('Content-type', 'application/json');
-      res.end(JSON.stringify(components));
-    });
-  } else { // Fall back to getting local file
-
-    // maps file extention to MIME types
-    const mimeType = {
-      '.ico': 'image/x-icon',
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.json': 'application/json',
-      '.css': 'text/css',
-      '.png': 'image/png',
-      '.jpg': 'image/jpeg',
-      '.wav': 'audio/wav',
-      '.mp3': 'audio/mpeg',
-      '.svg': 'image/svg+xml',
-      '.pdf': 'application/pdf',
-      '.doc': 'application/msword',
-      '.eot': 'appliaction/vnd.ms-fontobject',
-      '.ttf': 'aplication/font-sfnt'
-    };
-
-    // parse URL
-    const parsedUrl = url.parse(req.url);
-    // extract URL path
-    let pathname = jsonDirectory + `${parsedUrl.pathname}`;
-    fs.exists(pathname, function (exist) {
-      if(!exist) {
-        // if the file is not found, return 404
-        res.statusCode = 404;
-        res.end(`File ${pathname} not found!`);
-        return;
-      }
-      // if is a directory, then look for index.html
-      if (fs.statSync(pathname).isDirectory()) {
-        pathname += '/index.html';
-      }
-      // read file from file system
-      fs.readFile(pathname, function(err, data){
-        if(err){
-          res.statusCode = 500;
-          res.end(`Error getting the file: ${err}.`);
-        } else {
-          // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-          const ext = path.parse(pathname).ext;
-          // if the file is found, set Content-type and send data
-          res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-          res.end(data);
-        }
-      });
-    });
+  if (req.method == "GET" && req.url == "/components") {
+    getListOfJSONFiles(req, res);
+  } else {
+    getFileContents(req, res);
+    
   }
 }).listen(parseInt(port));
 
-console.log(`Server listening on port ${port}`);
+console.log("Server listening on port: " + port);
 
-getAvailableComponents(function(components) {
-  console.log('\nAvailable Components:\n' + components.join('\n') + '\n');
+fileUtil.getFilesInDirectory(jsonDirectory, false, function(code, fileNames, err) {
+  console.log((fileNames || []).length + " json files available.");
 });
---
+++ Components/file-util.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+
+const mimeType = {
+  ".ico": "image/x-icon",
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".wav": "audio/wav",
+  ".mp3": "audio/mpeg",
+  ".svg": "image/svg+xml",
+  ".pdf": "application/pdf",
+  ".doc": "application/msword",
+  ".eot": "appliaction/vnd.ms-fontobject",
+  ".ttf": "aplication/font-sfnt"
+};
+
+module.exports = {
+
+	/**
+	completion format: (code: 200, data: {...}, contentType: "", err: "")
+	*/
+	getContentsOfFile: function(pathname, completion) {
+
+		fs.exists(pathname, function (exist) {
+
+			// Return 404 if the file is not found.
+			if (!exist) {
+				completion(404, null, null, "File " + pathname + " not found!");
+				return 
+			}
+
+			// If is a directory, then look for index.html
+			if (fs.statSync(pathname).isDirectory()) {
+				pathname += "/index.html";
+			}
+			
+			// Read the file
+			fs.readFile(pathname, function(err, data){
+				if (err){
+					completion(500, null, null, "Error reading the file: ${err}.");
+				} else {
+					// Extract the content type from the pathname.
+					const ext = path.parse(pathname).ext;
+					const contentMimeType = mimeType[ext] || "text/plain";
+
+					completion(200, data, contentMimeType, null);
+				}
+			});
+		});
+	},
+
+	/**
+	completion format: (code: 200, files: [...], err: "")
+	*/
+	getFilesInDirectory: function(directory, stripExtension, completion) {
+		fs.readdir(directory, (err, files) => {
+			if (err) {
+				completion(500, null, "Unable to find directory.");
+				return;
+			}
+
+			let fileNames = []
+			files.forEach(file => {
+				if (stripExtension) {
+					fileNames.push(file.split(".")[0]);
+				} else {
+					fileNames.push(file);
+				}
+			});
+			completion(200, fileNames, null);
+		});
+
+	}
+};
