fc31296b9dc8067e34eabe0df7bc47b3aa0031cc
Mitchell Morgan <msmorgan90@gmail.com>
2017-05-05 08:45:08 -0400

    Updating console logs

--- run_template_server.sh
+++ run_template_server.sh
@@ -5,10 +5,8 @@ SERVER_PATH=$WORKING_DIR"/template-server"
 ESCAPED_SERVER_PATH=$(printf %q "$SERVER_PATH")
 SERVER_NAME="server.js"
 
-echo "Moving to directory: " $ESCAPED_SERVER_PATH
 cd $ESCAPED_SERVER_PATH
 
 echo "Running Node Server: " $SERVER_NAME
-echo ""
 
 node $SERVER_NAME
