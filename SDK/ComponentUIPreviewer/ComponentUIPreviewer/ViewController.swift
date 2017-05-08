bcf9791b0cc7212c214a78f48830650ffd033552
Mitchell Morgan <msmorgan90@gmail.com>
2017-05-08 07:46:26 -0400

    Adding project for previewing the component UI

---
+++ SDK/ComponentUIPreviewer/ComponentUIPreviewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComponentUIPreviewer
+//
+//  Created by Mitchell Morgan on 5/8/17.
+//  Copyright © 2017 ASAPP, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
