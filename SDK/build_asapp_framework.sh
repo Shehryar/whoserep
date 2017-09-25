6cdfe8f297f1a963796eea76d3ddc25174654499
Hans Hyttinen <hans@asapp.com>
2017-09-25 17:09:03 -0400

    don't delete Carthage build directory after building framework

--- SDK/build_asapp_framework.sh
+++ SDK/build_asapp_framework.sh
@@ -4,7 +4,8 @@ FRAMEWORK_NAME="ASAPP.framework"
 WORKING_DIR=$(pwd)
 BUILD_DIR=$WORKING_DIR"/Build"
 CARTHAGE_DIR=$WORKING_DIR"/Carthage"
-CARTHAGE_FRAMEWORK_PATH=$CARTHAGE_DIR"/Build/iOS/"$FRAMEWORK_NAME
+CARTHAGE_BUILD_DIR=$CARTHAGE_DIR"/Build"
+CARTHAGE_FRAMEWORK_PATH=$CARTHAGE_BUILD_DIR"/iOS/"$FRAMEWORK_NAME
 BUILD_FRAMEWORK_PATH=$BUILD_DIR"/"$FRAMEWORK_NAME
 
 echo ""
@@ -17,7 +18,7 @@ cd $WORKING_DIR
 echo ""
 echo "Cleaning Prior Builds"
 
-rm -rf $CARTHAGE_DIR
+rm -rf $CARTHAGE_BUILD_DIR
 rm -rf $BUILD_DIR
 
 echo ""
@@ -32,13 +33,8 @@ echo "Build succeeded. Copying to working directory"
 cd $WORKING_DIR
 mkdir $BUILD_DIR
 cd $WORKING_DIR
-# cp -r Carthage/Build/iOS/ASAPP.framework $BUILD_DIR"/ASAPP.framework"
 cp -r $CARTHAGE_FRAMEWORK_PATH $BUILD_FRAMEWORK_PATH
 cd $WORKING_DIR
-
-echo ""
-echo "Cleaning up mess"
-rm -rf $CARTHAGE_DIR
 
 echo "New framework available at: " $BUILD_FRAMEWORK_PATH
 open -R $BUILD_FRAMEWORK_PATH
