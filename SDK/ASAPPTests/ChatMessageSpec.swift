56234cc84b528e894ce4aa44c5750351f37a23a3
Hans E Hyttinen <31215539+hans-asapp@users.noreply.github.com>
2017-10-13 13:11:47 -0400

    SDK-313: more ChatMessage unit tests (#78)
    * tests for init and getAutoSelectQuickReply
    
    * test .fromJSON(_:with:) with legacy and new JSON
    
    * remove outdated and unused sample JSON files

--- SDK/ASAPPTests/ChatMessageSpec.swift
+++ SDK/ASAPPTests/ChatMessageSpec.swift
@@ -13,18 +13,147 @@ import Nimble
 class ChatMessageSpec: QuickSpec {
     override func spec() {
         describe("ChatMessage") {
-            describe(".fromLegacySRSJSON(_:with:)") {
-                let eventMetadata = EventMetadata(
-                    isReply: true,
-                    isAutomatedMessage: true,
-                    eventId: 0,
-                    eventType: .srsResponse,
-                    issueId: 1,
-                    sendTime: Date())
+            let metadata = EventMetadata(
+                isReply: true,
+                isAutomatedMessage: true,
+                eventId: 0,
+                eventType: .srsResponse,
+                issueId: 1,
+                sendTime: Date())
+            
+            describe(".init(text:attachment:quickReplies:metadata") {
+                let attachment = ChatMessageAttachment(content: "")
                 
+                context("without text, an attachment, and quickReplies") {
+                    it("is nil") {
+                        let msg = ChatMessage(text: nil, attachment: nil, quickReplies: nil, metadata: metadata)
+                        expect(msg).to(beNil())
+                    }
+                }
+                
+                context("without text") {
+                    it("is not nil") {
+                        let msg = ChatMessage(text: nil, attachment: attachment, quickReplies: [:], metadata: metadata)
+                        expect(msg).toNot(beNil())
+                    }
+                }
+                
+                context("without an attachment") {
+                    it("is not nil") {
+                        let msg = ChatMessage(text: "foo", attachment: nil, quickReplies: [:], metadata: metadata)
+                        expect(msg).toNot(beNil())
+                    }
+                }
+                
+                context("without quickReplies") {
+                    it("is not nil") {
+                        let msg = ChatMessage(text: "foo", attachment: attachment, quickReplies: nil, metadata: metadata)
+                        expect(msg).toNot(beNil())
+                    }
+                }
+                
+                context("with an empty quickReplies array") {
+                    it("has a nil quickReplies property") {
+                        let msg = ChatMessage(text: "", attachment: attachment, quickReplies: [:], metadata: metadata)
+                        expect(msg).toNot(beNil())
+                        expect(msg!.quickReplies).to(beNil())
+                    }
+                }
+                
+                context("with an attachment quick reply") {
+                    it("has the attachment quick reply") {
+                        let attachment = ChatMessageAttachment(content: Component(id: "a", name: "a", value: "foo", isChecked: nil, style: ComponentStyle(), styles: nil, content: nil) as Any)
+                        let quickReply = QuickReply(title: "bar", action: Action(content: "baz")!)
+                        let otherQuickReply = QuickReply(title: "alpha", action: Action(content: "beta")!)
+                        let msg = ChatMessage(text: "", attachment: attachment, quickReplies: [
+                            "foo": [quickReply],
+                            "other": [otherQuickReply]
+                        ], metadata: metadata)
+                        expect(msg).toNot(beNil())
+                        expect(msg!.quickReplies).toNot(beNil())
+                        expect(msg!.quickReplies).to(equal([quickReply]))
+                    }
+                }
+            }
+            
+            describe(".getAutoSelectQuickReply()") {
+                context("without quickReplies") {
+                    it("returns nil") {
+                        let msg = ChatMessage(text: nil, attachment: nil, quickReplies: [:], metadata: metadata)
+                        expect(msg!.getAutoSelectQuickReply()).to(beNil())
+                    }
+                }
+                
+                context("without any auto-select quick replies") {
+                    it("returns nil") {
+                        let msg = ChatMessage(text: nil, attachment: nil, quickReplies: [
+                            "foo": [QuickReply(title: "bar", action: Action(content: "")!)]
+                        ], metadata: metadata)
+                        expect(msg!.getAutoSelectQuickReply()).to(beNil())
+                    }
+                }
+                
+                context("with two auto-select quick replies") {
+                    it("returns the first one") {
+                        let alpha = QuickReply(title: "beta", action: Action(content: "")!, isAutoSelect: true)
+                        let gamma = QuickReply(title: "delta", action: Action(content: "")!, isAutoSelect: true)
+                        let msg = ChatMessage(text: nil, attachment: nil, quickReplies: [
+                            "alpha": [alpha],
+                            "gamma": [gamma]
+                        ], metadata: metadata)
+                        let result = msg!.getAutoSelectQuickReply()
+                        expect(result).toNot(beNil())
+                        expect(result).to(equal(alpha))
+                    }
+                }
+            }
+            
+            describe(".fromJson(_:with:)") {
+                context("without json") {
+                    it("returns nil") {
+                        let msg = ChatMessage.fromJSON(nil, with: metadata)
+                        expect(msg).to(beNil())
+                    }
+                }
+                
+                context("with a json argument that isn't a dictionary") {
+                    it("returns nil") {
+                        let msg = ChatMessage.fromJSON("just a string", with: metadata)
+                        expect(msg).to(beNil())
+                    }
+                }
+                
+                context("with legacy JSON") {
+                    it("returns a proper ChatMessage") {
+                        let json = TestUtil.jsonForFile(named: "add-credit-card")
+                        let msg = ChatMessage.fromJSON(json, with: metadata)
+                        expect(msg).toNot(beNil())
+                        expect(msg!.text).to(contain("add a credit card"))
+                        
+                        let action = msg!.quickReplies?.first?.action as? DeepLinkAction
+                        expect(action).toNot(beNil())
+                        expect(action!.name).to(equal("payment"))
+                    }
+                }
+                
+                context("with JSON containing a quick reply") {
+                    it("returns a ChatMessage with a quick reply") {
+                        let json = TestUtil.jsonForFile(named: "security-pin")
+                        let msg = ChatMessage.fromJSON(json, with: metadata)
+                        expect(msg).toNot(beNil())
+                        expect(msg!.text).to(contain("security PIN"))
+                        
+                        let action = msg!.quickReplies?.first?.action as? ComponentViewAction
+                        expect(action).toNot(beNil())
+                        expect(action!.name).to(contain("new_pin"))
+                    }
+                }
+            }
+            
+            describe(".fromLegacySRSJSON(_:with:)") {
                 context("without valid JSON") {
                     it("returns nil") {
-                        let msg = ChatMessage.fromLegacySRSJSON(nil, with: eventMetadata)
+                        let msg = ChatMessage.fromLegacySRSJSON(nil, with: metadata)
                         expect(msg).to(beNil())
                     }
                 }
@@ -32,15 +161,15 @@ class ChatMessageSpec: QuickSpec {
                 context("with contentType of carousel") {
                     it("returns nil") {
                         let json = TestUtil.jsonForFile(named: "phone-plan-upgrade")
-                        let msg = ChatMessage.fromLegacySRSJSON(json, with: eventMetadata)
+                        let msg = ChatMessage.fromLegacySRSJSON(json, with: metadata)
                         expect(msg).to(beNil())
                     }
                 }
                 
                 context("without a message component") {
                     it("returns nil") {
-                        let json = TestUtil.jsonForFile(named: "transaction-history")
-                        let msg = ChatMessage.fromLegacySRSJSON(json, with: eventMetadata)
+                        let json = TestUtil.jsonForFile(named: "live-chat-begin")
+                        let msg = ChatMessage.fromLegacySRSJSON(json, with: metadata)
                         expect(msg).to(beNil())
                     }
                 }
@@ -51,7 +180,7 @@ class ChatMessageSpec: QuickSpec {
                     
                     beforeEach {
                         json = TestUtil.jsonForFile(named: "add-credit-card")
-                        msg = ChatMessage.fromLegacySRSJSON(json, with: eventMetadata)
+                        msg = ChatMessage.fromLegacySRSJSON(json, with: metadata)
                     }
                     
                     it("has Make a Payment as its first quick reply") {
