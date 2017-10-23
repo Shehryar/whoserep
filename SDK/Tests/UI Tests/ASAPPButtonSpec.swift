ef585ddb1e631e60bbbeda6bc9d736876eeca8a7
Hans E Hyttinen <31215539+hans-asapp@users.noreply.github.com>
2017-10-23 11:24:17 -0400

    SDK-364: start snapshot testing (#91)
    * add ASAPPButton tests
    * add BubbleView and Button tests
    * add ChatInputView tests
    * add QuickReplyCell tests

---
+++ SDK/Tests/UI Tests/ASAPPButtonSpec.swift
@@ -0,0 +1,67 @@
+//
+//  ASAPPButtonSpec.swift
+//  UI Tests
+//
+//  Created by Hans Hyttinen on 10/19/17.
+//  Copyright © 2017 ASAPP. All rights reserved.
+//
+
+import UIKit
+@testable import ASAPP
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+class ASAPPButtonSpec: QuickSpec {
+    override func spec() {
+        describe("ASAPPButton") {
+            var viewController: UIViewController!
+            
+            beforeSuite {
+                FBSnapshotTest.setReferenceImagesDirectory(
+                    ProcessInfo.processInfo.environment["FB_REFERENCE_IMAGE_DIR"]!)
+                
+                let window = UIWindow(frame: UIScreen.main.bounds)
+                window.rootViewController = UIViewController()
+                window.makeKeyAndVisible()
+                
+                viewController = window.rootViewController
+                let config = ASAPPConfig(appId: "test", apiHostName: "test.example.com", clientSecret: "test")
+                ASAPP.initialize(with: config)
+                ASAPP.user = ASAPPUser(userIdentifier: "test", requestContextProvider: {
+                    return [:]
+                }, userLoginHandler: { _ in })
+            }
+            
+            context("on its own") {
+                beforeEach {
+                    ASAPP.styles = ASAPPStyles()
+                }
+                
+                context("with default styles") {
+                    it("has a valid snapshot") {
+                        let button = ASAPP.createChatButton(appCallbackHandler: { _, _ in }, presentingViewController: viewController)
+                        expect(button).to(haveValidSnapshot())
+                        expect(button).to(haveValidDynamicTypeSnapshot())
+                    }
+                }
+                
+                context("with a custom text color") {
+                    it("has a valid snapshot") {
+                        ASAPP.styles.colors.helpButtonText = .black
+                        let button = ASAPP.createChatButton(appCallbackHandler: { _, _ in }, presentingViewController: viewController)
+                        expect(button).to(haveValidSnapshot())
+                    }
+                }
+                
+                context("with a custom background color") {
+                    it("has a valid snapshot") {
+                        ASAPP.styles.colors.helpButtonBackground = .blue
+                        let button = ASAPP.createChatButton(appCallbackHandler: { _, _ in }, presentingViewController: viewController)
+                        expect(button).to(haveValidSnapshot())
+                    }
+                }
+            }
+        }
+    }
+}
