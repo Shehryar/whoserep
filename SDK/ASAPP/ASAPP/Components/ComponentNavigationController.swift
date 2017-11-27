78e1c4d70dc8a1a02e2bf26a9f3024261df922ed
Hans E Hyttinen <31215539+hans-asapp@users.noreply.github.com>
2017-11-27 11:30:55 -0500

    [SDK-601] Fix keyboard covering TextInput upon focus in a "full" displayStyle ComponentView (#119)

--- SDK/ASAPP/ASAPP/Components/ComponentNavigationController.swift
+++ SDK/ASAPP/ASAPP/Components/ComponentNavigationController.swift
@@ -12,6 +12,9 @@ class ComponentNavigationController: UINavigationController, UpdatableFrames {
 
     let presentationAnimator = ModalCardPresentationAnimator()
     
+    private var keyboardObserver: KeyboardObserver?
+    private var keyboardHeight: CGFloat = 0.0
+    
     var displayStyle: ComponentViewDisplayStyle = .full {
         didSet {
             useCustomPresentation = displayStyle == .inset
@@ -34,7 +37,21 @@ class ComponentNavigationController: UINavigationController, UpdatableFrames {
                 modalPresentationStyle = .fullScreen
                 transitioningDelegate = nil
                 presentationAnimator.tapToDismissEnabled = false
+                keyboardObserver = KeyboardObserver()
+                keyboardObserver!.delegate = self
             }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if isBeingPresented {
+            keyboardObserver?.registerForNotifications()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if isBeingDismissed {
+            keyboardObserver?.deregisterForNotification()
         }
     }
     
@@ -42,9 +59,37 @@ class ComponentNavigationController: UINavigationController, UpdatableFrames {
     
     func updateFrames() {
         for viewController in viewControllers {
-            if let updateFramesVC = viewController as? UpdatableFrames {
-                updateFramesVC.updateFrames()
+            if let updatableFramesVC = viewController as? UpdatableFrames {
+                updatableFramesVC.updateFrames()
             }
+        }
+    }
+}
+
+extension ComponentNavigationController: KeyboardObserverDelegate {
+    func keyboardWillUpdateVisibleHeight(_ height: CGFloat, withDuration duration: TimeInterval, animationCurve: UIViewAnimationOptions) {
+        guard height != keyboardHeight else {
+            return
+        }
+        
+        keyboardHeight = height
+        
+        if let updatableFramesVC = visibleViewController as? UpdatableFrames,
+           let view = visibleViewController?.view {
+            let newHeight = view.bounds.height - keyboardHeight
+            
+            UIView.animate(
+                withDuration: duration,
+                delay: 0,
+                options: animationCurve,
+                animations: {
+                    updatableFramesVC.updateFrames()
+                    
+                    var frame = view.frame
+                    frame.size.height = newHeight
+                    view.frame = frame
+                    view.layoutIfNeeded()
+                }, completion: nil)
         }
     }
 }
