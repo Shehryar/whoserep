e45668b5392786b7b25d019aa342b0c8594b9fca
Hans Hyttinen <hans@asapp.com>
2017-12-19 12:01:14 -0500

    create directory for provisioning profiles

--- scripts/install_codesigning_credentials.sh
+++ scripts/install_codesigning_credentials.sh
@@ -6,7 +6,8 @@ security unlock-keychain -p circle circle.keychain
 security list-keychains -d user -s login.keychain circle.keychain
 security default-keychain -s circle.keychain
 security import Provisioning/ASAPP\ Dev\ Certificates.p12 -k circle.keychain -P $ASAPP_P12_PASSWORD -T /usr/bin/codesign -T /usr/bin/security
-cp Provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
+cp -v Provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k circle circle.keychain
 security find-identity -p codesigning
 
