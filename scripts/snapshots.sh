f00c67d7eae2df36ef12ca2408a8d9b48ccf651c
Hans E Hyttinen <31215539+hans-asapp@users.noreply.github.com>
2019-01-11 11:47:00 -0500

    [SDK-2971] Record entire scroll view with snapshots.sh (#557)
    * support JSON describing a message
    * use default styles
    * generate snapshot for entire contents of scroll view

--- scripts/snapshots.sh
+++ scripts/snapshots.sh
@@ -2,11 +2,12 @@
 
 open "SDK/Tests/Component UI Layout Tests"
 echo -e "\n\n****************"
-echo "Place .json files describing a Component View container in SDK/Tests/Component UI Layout Tests"
+echo "Place .json files describing a Component View container or a Chat Message in SDK/Tests/Component UI Layout Tests"
 echo "Snapshots are generated in the Images directory."
+echo "If a Component View container describes a Scroll View, a snapshot of the entirety of its contents will be generated."
 echo "If you add a .json file and run snapshots.sh, the test runner will generate snapshots and report a failure."
 echo "Run it again to have it validate the snapshots and succeed."
 echo -e "****************\n\n"
 
-xcodebuild test -quiet -workspace SDK/ASAPP.xcworkspace -scheme 'Component UI Layout Tests' -destination 'platform=iOS Simulator,OS=12.0,name=iPhone SE' SKIP_LINTING=1
+xcodebuild test -quiet -workspace SDK/ASAPP.xcworkspace -scheme 'Component UI Layout Tests' -destination 'platform=iOS Simulator,OS=12.1,name=iPhone SE' SKIP_LINTING=1
 
